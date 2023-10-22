26ed9a822fcc017647aab52f7eae704fc80b1484
Suim <143478016+suim-park@users.noreply.github.com>
2023-10-22 22:20:19 +0000

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -2,11 +2,17 @@ from library.average import average, calculate_time_memory
 
 data_path = "births14.csv"
 
-def test_average(path):
-    result = average(path)
+def test_average():
+    result = average("births14.csv")
     expected_result = 7.19816
 
     assert result == expected_result, "Test has failed."
 
+def test_calculate_time_memory():
+    result = calculate_time_memory("births14.csv")
+    
+    assert result is not None, "Test has failed."
+
 if __name__ == "__main__":
-    test_average(data_path)
+    test_average()
+    test_calculate_time_memory()
