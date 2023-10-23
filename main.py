ab2fbc7d175f264d45b0856bcb48be82697d1467
Suim <143478016+suim-park@users.noreply.github.com>
2023-10-23 04:08:02 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,12 @@ from library.average import average, calculate_time_memory
 data_path = "births14.csv"
 
 if __name__ == "__main__":
-    average(data_path)
-    calculate_time_memory(data_path)
+    data_path = "/workspaces/Mini-Project-8/births14.csv"  # Updated with the actual CSV file path
+
+    avg_weight = average(data_path)
+    print(f"Average weight: {avg_weight:.5}")
+
+    end_mem_usage, elapsed_time = calculate_time_memory(data_path)
+
+    print(f"Final Memory Usage: {end_mem_usage} kilobytes")
+    print(f"Total Elapsed Time: {elapsed_time:.7} seconds")
