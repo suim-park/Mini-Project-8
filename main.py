7b563f8985b2354015b1c3351f4f598c30c15f89
Suim <143478016+suim-park@users.noreply.github.com>
2023-10-23 04:09:16 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from library.average import average, calculate_time_memory
 data_path = "births14.csv"
 
 if __name__ == "__main__":
-    data_path = "/workspaces/Mini-Project-8/births14.csv"  # Updated with the actual CSV file path
+    data_path = "births14.csv"  # Updated with the actual CSV file path
 
     avg_weight = average(data_path)
     print(f"Average weight: {avg_weight:.5}")
